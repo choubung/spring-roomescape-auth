4dcfea0393b88b2fb1e6cdd98a76f2a73688727a
choubung <choubung@sookmyung.ac.kr>
2026-06-17 16:36:54 +0900

    feat(service): 회원가입 및 로그인 비즈니스 로직(AuthService) 구현

---
+++ src/main/java/roomescape/service/AuthService.java
@@ -0,0 +1,43 @@
+package roomescape.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import roomescape.dao.UserDao;
+import roomescape.domain.auth.Token;
+import roomescape.domain.user.LoginId;
+import roomescape.domain.user.Password;
+import roomescape.domain.user.User;
+import roomescape.exception.AuthenticationException;
+import roomescape.exception.DuplicateException;
+import roomescape.exception.ResourceNotFoundException;
+import roomescape.global.auth.JwtProvider;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final UserDao userDao;
+    private final JwtProvider jwtProvider;
+
+    public User signUp(User user) {
+        if (userDao.existsByLoginId(user.getLoginId())) {
+            throw new DuplicateException("이미 존재하는 아이디입니다.");
+        }
+
+        Long savedId = userDao.create(user);
+
+        return userDao.findById(savedId)
+                .orElseThrow(() -> new ResourceNotFoundException("예약이 정상적으로 생성되지 않았습니다."));
+    }
+
+    public Token login(LoginId loginId, Password password) {
+        User user = userDao.findByLoginId(loginId)
+                .orElseThrow(() -> new ResourceNotFoundException("아아디 또는 비밀번호가 틀립니다."));
+
+        if (!user.getPassword().equals(password)) {
+            throw new AuthenticationException("아아디 또는 비밀번호가 틀립니다.");
+        }
+
+        return jwtProvider.generateToken(user);
+    }
+}
